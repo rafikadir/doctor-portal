09ccc3023141e035af0d3548df590d6e9c47bdf9
rafikadir <rafikadir2000@gmail.com>
2021-04-15 19:24:05 +0600

    add

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import {
   Route
 } from "react-router-dom";
 import Home from './Components/Home/Home/Home';
+import Appointments from './Components/Appointments/Appointment/Appointments';
+
 
 function App() {
   return (
     <div>
       <Router>
           <Switch>
+              <Route path="/appointment">
+                <Appointments></Appointments>
+              </Route>
               <Route path="/">
                 <Home></Home>
               </Route>
---
+++ src/Components/Appointments/Appointment/Appointments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointments = () => {
+    return (
+        <div>
+            <h1>This is Appointpent Page</h1>
+        </div>
+    );
+};
+
+export default Appointments;
